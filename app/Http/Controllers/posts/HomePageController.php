41ab0d1311ce38e19692b1aab1e963c510f6d25b
Bike-innocent <onyemaobichibuikeinnocent.com@gmail.com>
2024-07-22 08:34:16 -0800

    updating the mixedpost

---
+++ app/Http/Controllers/posts/HomePageController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Http\Controllers\posts;
+use App\Http\Controllers\Controller;
+
+
+use App\Models\Category;
+use App\Models\SubCategory;
+use Illuminate\Http\Request;
+
+class HomePageController extends Controller
+{
+   
+}
