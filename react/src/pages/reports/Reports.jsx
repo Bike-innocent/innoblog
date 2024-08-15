a9d32d7de080321de98da5132b5359a616cd6792
Bike-innocent <onyemaobichibuikeinnocent.com@gmail.com>
2024-08-14 16:08:16 -0800

    updated the reports and the display2

--- react/src/pages/reports/Reports.jsx
+++ react/src/pages/reports/Reports.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';  // Import Link from React Router
 import axiosInstance from '../../axiosInstance';
-
+//reports
 function Reports() {
     const [reports, setReports] = useState([]);
 
