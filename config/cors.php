c855d2d5a6a775008f2fd962445c6f74598cff28
Bike-innocent <onyemaobichibuikeinnocent.com@gmail.com>
2024-09-01 01:08:05 +0100

    updated the cors

--- config/cors.php
+++ config/cors.php
@@ -3,16 +3,13 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-<<<<<<< HEAD
-    'allowed_methods' => ['*'],   
+   'allowed_methods' => ['*'],   
    
     'allowed_origins' => ['https://innoblog.com.ng','http://localhost:5173'],
 
-=======
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://innoblog.com.ng','http://localhost:5173'],
->>>>>>> f3b7fa70e59778bcb3719d7bed83e026586272b9
+    
 //    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
