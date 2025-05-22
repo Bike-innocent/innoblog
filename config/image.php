959c90987ffe15d21c9974236aad5273aba18b6a
bike-innocent <onyemaobichibuikeinnocent.com@gmail.com>
2025-05-22 01:19:09 +0100

    updated cpanel images79

--- config/image.php
+++
@@ -1,7 +0,0 @@
-<?php
-// config/imae.php
-// config/image.php
-return [
-    'url' => env('IMAGE_URL', 'https://chibuikeinnocent.tech/post-images/'),
-    'avatar_url' => env('AVATAR_URL', 'https://chibuikeinnocent.tech/user-avatar/'),
-];
