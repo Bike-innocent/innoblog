4a0d9ebe03b48f6b8a3c3bb9ce84e295b5d1aba0
bike-innocent <onyemaobichibuikeinnocent.com@gmail.com>
2025-05-22 17:10:33 +0100

    updated cpanel images8jreessdddc

--- config/image.php
+++ config/image.php
@@ -1,10 +1,6 @@
 <?php
 // config/image.php
-// return [
-//     'url' => env('IMAGE_URL', 'https://chibuikeinnocent.tech/post-images/'),
-//     'avatar_url' => env('AVATAR_URL', 'https://chibuikeinnocent.tech/user-avatar/'),
-// ];
 return [
+    'url' => env('IMAGE_URL', 'https://chibuikeinnocent.tech/post-images/'),
     'avatar_url' => env('AVATAR_URL', 'https://chibuikeinnocent.tech/avatars/'),
-     'url' => env('IMAGE_URL', 'https://chibuikeinnocent.tech/post-images/'),
 ];
